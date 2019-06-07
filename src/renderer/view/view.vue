ebfc58aeab0e8936a2a324792b109b1c01c64462
Evan.G <seazeg@126.com>
2019-06-07 15:32:24 +0800

    Refactoring update

--- src/renderer/view/view.vue
+++ src/renderer/view/view.vue
@@ -9,19 +9,3 @@
         name: 'main-view'
     }
 </script>
-<style>
-    .logo {
-        width: 1000px;
-        height: 1000px;
-        background: url('../assets/change.png') no-repeat;
-        background-position: center;
-        background-size: 25%;
-        position: absolute;
-        top: 0;
-        left: 0;
-        bottom: 0;
-        right: 0;
-        margin: auto;
-        z-index: 0
-    }
-</style>
